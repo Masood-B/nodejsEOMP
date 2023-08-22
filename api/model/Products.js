bfd1649061765c0b7dd70c61229a80bb5537c85d
Masood-B <masoodbasardien123@gmail.com>
2023-08-22 11:04:02 +0200

    update

--- api/model/Products.js
+++ api/model/Products.js
@@ -28,6 +28,48 @@ class Products{
             })
         })
     }
+    async registerProduct(req, res){
+        const data = req.body
+        const query = `
+        INSERT INTO Products
+        SET ?;
+        `
+        db.query(query, [data], (err)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "You have register a product"
+            })
+        })
+    }
+    updateProduct(req, res){
+        const query = `
+        UPDATE Products
+        SET ?
+        WHERE prodID = ?;
+        `
+        db.query(query, [req.body, req.params.id],
+            (err)=>{
+                if (err) throw err
+                res.json({
+                    status: res.statusCode,
+                    msg: "Your products has been updated"
+                })
+            })
+    }
+    deleteProduct(req, res){
+        const query = `
+        DELETE FROM Products
+        WHERE prodID = ${req.params.id}
+        `
+        db.query(query, (err)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "The products have been removed"
+            })
+        })
+    }
 }
 
 module.exports = Products
