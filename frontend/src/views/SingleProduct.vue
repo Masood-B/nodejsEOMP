18e400d9cb72e59dec7390676463d769d125eadf
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-08-24 11:43:19 +0200

    other

--- frontend/src/views/SingleProduct.vue
+++ frontend/src/views/SingleProduct.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-<div class="card">
+<!-- <div class="card">
     <img :src="$route.query.prodUrl" alt="">
     <div class="card-title">
         {{ $route.query.prodName }}
+    </div>
+</div> -->
+
+<div>
+    <div class="card m-5" style="width: 18rem">
+        <img :src="$route.query.img" class="card-img-top" alt="">
+        <div class="card-title text-white">
+            {{ $route.query.product }}
+        </div>
     </div>
 </div>
     </div>
