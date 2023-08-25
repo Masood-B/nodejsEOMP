18d5e32c045f1def1958ab80e74caf356321372b
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-08-25 18:06:10 +0200

    yah

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,32 +1,31 @@
 <template>
-    <div>
-        <!-- <nav class="navbar navbar-expand-lg">
-
-            <div class="container-fluid">
+    <!-- <nav class="navbar navbar-expand-lg">
+        
+        <div class="container-fluid">
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                  <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li>
                     <router-link class="head" to="/">Home</router-link>
-                  </li>
-                  <li>
+                </li>
+                <li>
                     <router-link class="head" to="/about">About Us</router-link>
+                </li>
+                  <li>
+                      <router-link class="head" to="/admin">Admin</router-link>
                   </li>
                   <li>
-                    <router-link class="head" to="/admin">Admin</router-link>
-                  </li>
+                      <router-link class="head" to="/products">Products</router-link>
+                    </li>
                   <li>
-                    <router-link class="head" to="/products">Products</router-link>
-                  </li>
-                  <li>
-                    <router-link class="head" to="/contact">Contact</router-link>
-                  </li>
+                      <router-link class="head" to="/contact">Contact</router-link>
+                    </li>
                 </ul>
                 <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
               </div>
             </div>
@@ -34,55 +33,56 @@
 
           <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">HorrorHaven</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="#">HorrorHaven</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-              </button>
+            </button>
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                  <ul class="navbar-nav">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="./index.html" style="margin-left:60rem ;">Home</a>
-                  </li>
-                  <li >
+                </li>
+                <li >
                     <a class="nav-link" href="./script/about.html">About</a>
 
                     <router-link class="head" to="/about">About Us</router-link>
 
-                  </li>
-                  <li class="nav-item">
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="./script/product.html">Product</a>
-                  </li>
-
-                  <li class="nav-item">
+                </li>
+                
+                <li class="nav-item">
                     <a class="nav-link" href="./script/product.html">Admin</a>
-                  </li>
-
-
+                </li>
+                
+                
                   <li class="nav-item">
                     <a class="nav-link" href="./script/product.html">Contact</a>
                   </li>
-
+                  
                   <li class="nav-item">
-                    <a class="nav-link" href="./script/product.html">Skills</a>
-                  </li>
+                      <a class="nav-link" href="./script/product.html">Skills</a>
+                    </li>
 
                   <form class="d-flex" role="search">
                     <i class="bi bi-search" style="margin-top:0.5rem;color:white;"><span style="margin-left:1.3rem;color:white;">SEARCH</span></i>
                 </form>
-                </ul>
+            </ul>
               </div>
             </div>
-          </nav> -->
-   
-          <nav class="navbar sticky-top navbar-expand-lg " >
-            <!-- <nav class="navbar sticky-top bg-body-tertiary" > -->
-
-
-        <!-- <a href="" class="logo">HorrorHaven</a> -->
-
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#" ><img src="" id="Lee"></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        </nav> -->
+        
+        <!-- <nav class="navbar sticky-top bg-body-tertiary" > -->
+            
+            
+            <!-- <a href="" class="logo">HorrorHaven</a> -->
+            
+            <div>
+                <div class="container-fluid">
+                    <nav class="navbar navbar-expand-lg  position-fixed bg-black ">
+                  <a class="navbar-brand" href="#" ><img src="" id="Lee" style="margin-right:3rem"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -113,10 +113,10 @@
                 </ul>
             </header>
               </div>
+            </nav>
             </div>
             <a class="navbar-brand" id="navpic" href="#"><img src="https://i.postimg.cc/jSDHxZyz/Screenshot-2023-08-22-121014.png" class="haven" alt=""></a>
 
-          </nav>
         </div> 
    
 </template>
@@ -155,20 +155,10 @@
     background: linear-gradient(45deg, red, rgb(244));
     color: rgb(253, 252, 252);
   }
-/*nav{
- background-color: #000;
-}
 
-
-
-.nav-link:hover {
- background-color: rgb(206, 22, 22);
- border-radius: 30%; 
-}
-.pic{
-   height: 1;
-   width: 10%
-}*/
+  .navbar{
+    z-index: 2;
+  }
 
 .haven{
  width: 8rem;   
